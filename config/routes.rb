7f755326661500d7e556a3dbb6cf69f7ce7cbb80
Michael Stafford <michaelstafford808@gmail.com>
2022-10-28 10:48:29 -1000

    updated rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
-  get "/even_movie", to: "movies#even_movies"
 
   resources :movies, only: [:index, :create, :update, :destroy]
   resources :genres, only:[:index, :create]
@@ -13,9 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # create a button 
-  # when the user clicks it, then console.log the movies that were released in even number years 
-  # Ex: movies released in 1994 & 2000
 
-  # create custom route, which would get a get request, path of "/even", to "movies#even"
 end
